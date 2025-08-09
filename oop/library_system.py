995bf3dd5926cdcddbfe3e19e65c4d59b48a9358
brianstatesthat <mbspeelman@gmail.com>
2025-08-09 20:32:58 +0200

    library system added to oop directory

---
+++ oop/library_system.py
@@ -0,0 +1,47 @@
+class Book:
+    def __init__(self, title: str, author: str):
+        """Initialize base Book with title and author."""
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        """Return string representation of the book."""
+        return f"'{self.title}' by {self.author}"
+
+
+class EBook(Book):
+    def __init__(self, title: str, author: str, file_size: int):
+        """Initialize EBook with additional file_size attribute."""
+        super().__init__(title, author)
+        self.file_size = file_size  # in KB
+
+    def __str__(self):
+        """Return string representation of the ebook."""
+        return f"{super().__str__()} [EBook, {self.file_size}KB]"
+
+
+class PrintBook(Book):
+    def __init__(self, title: str, author: str, page_count: int):
+        """Initialize PrintBook with additional page_count attribute."""
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __str__(self):
+        """Return string representation of the print book."""
+        return f"{super().__str__()} [Print, {self.page_count} pages]"
+
+
+class Library:
+    def __init__(self):
+        """Initialize Library with an empty list of books."""
+        self.books = []
+
+    def add_book(self, book: Book):
+        """Add a book (Book, EBook, or PrintBook) to the library."""
+        self.books.append(book)
+
+    def list_books(self):
+        """Print details of all books in the library."""
+        print("Library Contents:")
+        for book in self.books:
+            print(f"- {book}")
